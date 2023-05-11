e34a56a1f51ce5012bcc2b20963431066ef0a195
John W <54917445+Minedbender@users.noreply.github.com>
2023-05-11 19:03:30 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Solidjs_template
+A solidjs Typescript template to start new projects with
---
+++ src/routes/App.tsx
@@ -0,0 +1,12 @@
+import { Component } from 'solid-js';
+const App: Component = () => {
+  return (
+    <div>
+      <header>
+        <h1>Hello World!</h1>
+      </header>
+    </div>
+  );
+};
+
+export default App;
